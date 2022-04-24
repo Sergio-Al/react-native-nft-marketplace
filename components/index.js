eec0851e51f4f7a834327f3a857073519f3c7dce
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2022-04-23 20:02:22 -0400

    All UI finished

--- components/index.js
+++ components/index.js
@@ -3,4 +3,17 @@ import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton };
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
